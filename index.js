b227c2170d344b07e9f75d85f876a2eff14f3694
MUYU <755596386@qq.com>
2024-11-22 15:58:32 +0800

    1

--- index.js
+++ index.js
@@ -1,13 +1,32 @@
 // server.js
 const http = require('http');
+const url = require('url');
 
 // 创建服务器并响应请求
 const server = http.createServer((req, res) => {
+  // 获取请求的 URL 和方法
+  const { pathname } = url.parse(req.url, true);
+  const method = req.method;
+
   // 设置响应头
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  
-  // 响应客户端
-  res.end('Hello, Node.js server!');
+
+  // 根据不同的路由和请求方法响应不同内容
+  if (method === 'GET') {
+    if (pathname === '/') {
+      res.end('Welcome to the home page!');
+    } else if (pathname === '/hello') {
+      res.end('Hello, World!');
+    } else if (pathname === '/about') {
+      res.end('This is a sample Node.js server.');
+    } else {
+      res.writeHead(404, { 'Content-Type': 'text/plain' });
+      res.end('404 Not Found');
+    }
+  } else {
+    res.writeHead(405, { 'Content-Type': 'text/plain' });
+    res.end(`Method ${method} Not Allowed`);
+  }
 });
 
 // 启动服务器并监听端口
